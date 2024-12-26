8adfcbd4f4b6abbdfb44ca05451e0a61a376774a
Shahab Shahrashoob <117520004+Shahab-Shahrashoob@users.noreply.github.com>
2024-12-26 21:54:47 +0330

    Fixing VihecleLib

--- Class/VehicleLib.py
+++ Class/VehicleLib.py
@@ -13,90 +13,79 @@ Root = None
 
 def setup_vehicle_management(root, frame):
     Root = root
-    display_frame = ttk.Frame(frame)
-    display_frame.grid(row=14, column=0, columnspan=2, sticky="nsew")
-    display_frame.grid_rowconfigure(0, weight=1)
-    display_frame.grid_columnconfigure(0, weight=1)
-    ttk.Label(frame, text="Plate:").grid(row=0, column=0, padx=10, pady=5)
-    plate_entry = ttk.Entry(frame)
-    plate_entry.grid(row=0, column=1, padx=10, pady=5)
+    
+    # Create a frame for input fields and buttons
+    input_frame = ttk.Frame(frame)
+    input_frame.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
+    
+    # Add input fields and labels
+    ttk.Label(input_frame, text="Plate:").grid(row=0, column=0, padx=10, pady=5, sticky="e")
+    plate_entry = ttk.Entry(input_frame)
+    plate_entry.grid(row=0, column=1, padx=10, pady=5, sticky="w")
 
-    ttk.Label(frame, text="Type:").grid(row=1, column=0, padx=10, pady=5)
-    type_entry = ttk.Entry(frame)
-    type_entry.grid(row=1, column=1, padx=10, pady=5)
+    ttk.Label(input_frame, text="Type:").grid(row=1, column=0, padx=10, pady=5, sticky="e")
+    type_entry = ttk.Entry(input_frame)
+    type_entry.grid(row=1, column=1, padx=10, pady=5, sticky="w")
 
-    ttk.Label(frame, text="Location:").grid(row=2, column=0, padx=10, pady=5)
-    location_entry = ttk.Entry(frame)
-    location_entry.grid(row=2, column=1, padx=10, pady=5)
+    ttk.Label(input_frame, text="Location:").grid(row=2, column=0, padx=10, pady=5, sticky="e")
+    location_entry = ttk.Entry(input_frame)
+    location_entry.grid(row=2, column=1, padx=10, pady=5, sticky="w")
 
-    ttk.Button(
-        frame,
-        text="Add Vehicle",
-        command=lambda: add_vehicle(
-            plate_entry.get(), type_entry.get(), location_entry.get()
-        ),
-    ).grid(row=3, column=0, columnspan=2, pady=10)
+    ttk.Label(input_frame, text="Fuel Consumption:").grid(row=3, column=0, padx=10, pady=5, sticky="e")
+    fuel_consumption_entry = ttk.Entry(input_frame)
+    fuel_consumption_entry.grid(row=3, column=1, padx=10, pady=5, sticky="w")
 
-    display_button = ttk.Button(
-        frame,
-        text="Display Current Vehicles",
-        command=lambda: display_vehicles(display_frame),
-    )
-    display_button.grid(row=4, column=0, columnspan=2, pady=10)
+    ttk.Label(input_frame, text="Capacity (for Buses):").grid(row=4, column=0, padx=10, pady=5, sticky="e")
+    capacity_entry = ttk.Entry(input_frame)
+    capacity_entry.grid(row=4, column=1, padx=10, pady=5, sticky="w")
 
-    filter_label = ttk.Label(frame, text="Filter By Type:")
-    filter_label.grid(row=5, column=0, padx=10, pady=5)
-    filter_entry = ttk.Entry(frame)
-    filter_entry.grid(row=5, column=1, padx=10, pady=5)
-    filter_button = ttk.Button(
-        frame,
-        text="Filter Vehicles",
-        command=lambda: filter_vehicles(filter_entry.get(), display_frame),
-    )
-    filter_button.grid(row=6, column=0, columnspan=2, pady=10)
+    ttk.Label(input_frame, text="Model (for Cars):").grid(row=5, column=0, padx=10, pady=5, sticky="e")
+    model_entry = ttk.Entry(input_frame)
+    model_entry.grid(row=5, column=1, padx=10, pady=5, sticky="w")
 
-    plate_label_delete = ttk.Label(frame, text="Plate to Delete:")
-    plate_label_delete.grid(row=7, column=0, padx=10, pady=5)
-    plate_entry_delete = ttk.Entry(frame)
-    plate_entry_delete.grid(row=7, column=1, padx=10, pady=5)
-    delete_button = ttk.Button(
-        frame,
-        text="Delete Vehicle",
-        command=lambda: delete_vehicle(plate_entry_delete.get(), display_frame),
-    )
-    delete_button.grid(row=8, column=0, columnspan=2, pady=10)
+    ttk.Label(input_frame, text="Max Speed (for MotorCycles):").grid(row=6, column=0, padx=10, pady=5, sticky="e")
+    max_speed_entry = ttk.Entry(input_frame)
+    max_speed_entry.grid(row=6, column=1, padx=10, pady=5, sticky="w")
 
-    plate_label_update = ttk.Label(frame, text="Plate to Update:")
-    plate_label_update.grid(row=9, column=0, padx=10, pady=5)
-    plate_entry_update = ttk.Entry(frame)
-    plate_entry_update.grid(row=9, column=1, padx=10, pady=5)
+    ttk.Label(input_frame, text="Class (for Trucks):").grid(row=7, column=0, padx=10, pady=5, sticky="e")
+    class_entry = ttk.Entry(input_frame)
+    class_entry.grid(row=7, column=1, padx=10, pady=5, sticky="w")
 
-    type_label_update = ttk.Label(frame, text="New Type:")
-    type_label_update.grid(row=10, column=0, padx=10, pady=5)
-    type_entry_update = ttk.Entry(frame)
-    type_entry_update.grid(row=10, column=1, padx=10, pady=5)
+    ttk.Button(input_frame, text="Add Vehicle", command=lambda: add_vehicle_to_db(
+        plate_entry.get(), type_entry.get(), location_entry.get(), fuel_consumption_entry.get(),
+        capacity_entry.get(), model_entry.get(), max_speed_entry.get(), class_entry.get())
+    ).grid(row=8, column=0, columnspan=2, pady=10)
+    
+    ttk.Label(input_frame, text="Filter By Type:").grid(row=9, column=0, padx=10, pady=5, sticky="e")
+    filter_entry = ttk.Entry(input_frame)
+    filter_entry.grid(row=9, column=1, padx=10, pady=5, sticky="w")
+    ttk.Button(input_frame, text="Filter Vehicles", command=lambda: filter_vehicles(filter_entry.get(), display_frame)).grid(row=10, column=0, columnspan=2, pady=10)
 
-    location_label_update = ttk.Label(frame, text="New Location:")
-    location_label_update.grid(row=11, column=0, padx=10, pady=5)
-    location_entry_update = ttk.Entry(frame)
-    location_entry_update.grid(row=11, column=1, padx=10, pady=5)
+    ttk.Label(input_frame, text="Plate to Delete:").grid(row=11, column=0, padx=10, pady=5, sticky="e")
+    plate_entry_delete = ttk.Entry(input_frame)
+    plate_entry_delete.grid(row=11, column=1, padx=10, pady=5, sticky="w")
+    ttk.Button(input_frame, text="Delete Vehicle", command=lambda: delete_vehicle(plate_entry_delete.get(), display_frame)).grid(row=12, column=0, columnspan=2, pady=10)
 
-    update_button = ttk.Button(
-        frame,
-        text="Update Vehicle",
-        command=lambda: update_vehicle(
-            plate_entry_update.get(),
-            {"type": type_entry_update.get(), "location": location_entry_update.get()},
-            display_frame,
-        ),
-    )
-    update_button.grid(row=12, column=0, columnspan=2, pady=10)
+    ttk.Label(input_frame, text="Plate to Update:").grid(row=13, column=0, padx=10, pady=5, sticky="e")
+    plate_entry_update = ttk.Entry(input_frame)
+    plate_entry_update.grid(row=13, column=1, padx=10, pady=5, sticky="w")
 
-    logout_button = ttk.Button(
-        frame, text="Log Out", command=lambda: show_login_screen()
-    )
-    logout_button.grid(row=13, column=1, columnspan=2, pady=10)
+    ttk.Label(input_frame, text="New Type:").grid(row=14, column=0, padx=10, pady=5, sticky="e")
+    type_entry_update = ttk.Entry(input_frame)
+    type_entry_update.grid(row=14, column=1, padx=10, pady=5, sticky="w")
 
+    ttk.Label(input_frame, text="New Location:").grid(row=15, column=0, padx=10, pady=5, sticky="e")
+    location_entry_update = ttk.Entry(input_frame)
+    location_entry_update.grid(row=15, column=1, padx=10, pady=5, sticky="w")
+
+    ttk.Button(input_frame, text="Update Vehicle", command=lambda: update_vehicle(
+        plate_entry_update.get(), {"type": type_entry_update.get(), "location": location_entry_update.get()}, display_frame
+    )).grid(row=16, column=0, columnspan=2, pady=10)
+
+    ttk.Button(input_frame, text="Display Current Vehicles", command=lambda: display_vehicles(display_frame)).grid(row=17, column=0, columnspan=2, pady=10)
+
+    ttk.Button(input_frame, text="Back", command=lambda: main.show_Companies()).grid(row=18, column=0, columnspan=2, pady=10)
+    
     display_vehicles(display_frame)
 
 
